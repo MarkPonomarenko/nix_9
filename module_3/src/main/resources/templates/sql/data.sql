0d4768a0c6592aa6330ef749347aae9858fdaf27
Mark_Ponomarenko <ponomarenko.marke@gmail.com>
2022-01-23 17:06:59 +0200

    module 3 sql data

---
+++ module_3/src/main/resources/templates/sql/data.sql
@@ -0,0 +1,14 @@
+
+
+INSERT INTO users (`id`, `created`, `updated`, `name`, `password`, `phone`) VALUES (default, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Mark', '12344321', '0965374034');
+INSERT INTO users (`id`, `created`, `updated`, `name`, `password`, `phone`) VALUES (default, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Artem', '8888888', '7777344456');
+INSERT INTO users (`id`, `created`, `updated`, `name`, `password`, `phone`) VALUES (default, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Dima', 'useruser', '0642347045');
+INSERT INTO users (`id`, `created`, `updated`, `name`, `password`, `phone`) VALUES (default, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Liza', 'midus132', '0687456578');
+
+
+INSERT INTO accounts (`id`, `created`, `updated`, `balance`, `owner_id`) VALUES (default, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '900.0', '1');
+INSERT INTO accounts (`id`, `created`, `updated`, `balance`, `owner_id`) VALUES (default, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '190.0', '1');
+INSERT INTO accounts (`id`, `created`, `updated`, `balance`, `owner_id`) VALUES (default, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '90.0', '2');
+INSERT INTO accounts (`id`, `created`, `updated`, `balance`, `owner_id`) VALUES (default, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '700.0', '3');
+INSERT INTO accounts (`id`, `created`, `updated`, `balance`, `owner_id`) VALUES (default, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1003.0', '4');
+INSERT INTO accounts (`id`, `created`, `updated`, `balance`, `owner_id`) VALUES (default, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '345.0', '4');
